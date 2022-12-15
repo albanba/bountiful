845738853d829d986766fce0d6363100a9db3446
albanba <albanba@gmail.com>
2022-12-14 18:25:55 -0600

    1

--- scripts/form.js
+++ scripts/form.js
@@ -1,5 +1,7 @@
 document.getElementById("order").addEventListener("click", displayForm);
 
 function displayForm(){
-    
+    const name = document.querySelector("#name").value;
+    // document.getElementById('order').textContent = element[0]
+    console.log(name)
 }
