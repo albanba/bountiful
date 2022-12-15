9f42ff09dce71209442e7a590d42006d11c2b6f0
albanba <albanba@gmail.com>
2022-12-15 00:01:21 -0600

    1

--- scripts/form.js
+++ scripts/form.js
@@ -1,44 +1,32 @@
 
 
-document.querySelector("#submit").addEventListener("click", displayForm());
+document.querySelector("#submit").addEventListener("click", displayForm);
 
-// const p = document.createElement("p")
-// const form = []
+
+
 
 function displayForm(){
   
-    
-    const inputs = document.getElementById("fruitForm").elements;
-    
-    localStorage.setItem("Form", inputs);
-    
-    
-
-
-
-    // fname = document.querySelector("#name").value;
-
-    // // form.push(document.querySelector("#name").value);
-    // // form.push(document.querySelector("#email").value); 
-    // // form.push(document.querySelector("#phone").value);
-    // // form.push(document.querySelector("#option1").value);
-    // // form.push(document.querySelector("#option2").value);
-    // // form.push(document.querySelector("#option3").value);
-
-    // console.log(fname);
-    
-    
-    // p.innerHTML = `${fname}`;
-
-    // document.querySelector('.order').appendChild(p);
-
-    // for (let x in form) {
-    //     p.textContent = form[x];
-    //     document.querySelector('#order').appendChild(p);
-    //   }
-    
-
-
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const phone = document.querySelector("#phone").value;
+    const option1 = document.querySelector(".option1").value;
+    const option2 = document.querySelector(".option2").value;
+    const option3 = document.querySelector(".option3").value;
 
     
+      
+    localStorage.setItem('Name', name);
+    localStorage.setItem('Email', email);
+    localStorage.setItem('Phone', phone);
+    localStorage.setItem('Option1', option1);
+    localStorage.setItem('Option2', option2);
+    localStorage.setItem('Option3', option3);
+
+    const p = document.createElement("p");
+    
+    p.innerHTML = window.localStorage.getItem("Name");
+
+    document.querySelector('.order').appendChild(p);
+  
 }
